8485cbaf327bd75363ec3c75b40269b6ef9e8a01
Martin Ciz <martyciz@gmail.com>
2014-07-11 21:35:23 +0200

    added basic routes and partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 Rails.application.routes.draw do
-  get 'pages/home'
 
-  get 'pages/contact'
-
-  get 'pages/about'
-
-  get 'pages/help'
-
+  match '/contact', :to => 'pages#contact', :via => [:get, :post]
+  match '/about', :to => 'pages#about', :via => [:get, :post]
+  match '/help', :to => 'pages#help', :via => [:get, :post]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root :to => 'pages#home'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
