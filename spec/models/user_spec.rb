ec4fe2b8ddf63f252ba13a9e18789f49a1b985e9
Martin Ciz <martyciz@gmail.com>
2014-07-22 23:28:57 +0200

    added user microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -171,5 +171,20 @@ describe User do
         Micropost.find_by_id(micropost.id).should be_nil
       end
     end
+
+    it "should have a feed" do
+      @user.should respond_to(:feed)
+    end
+
+    it "should include the user's microposts" do
+      @user.feed.include?(@mp1).should be_truthy
+      @user.feed.include?(@mp2).should be_truthy
+    end
+
+    it "should not include a different user's microposts" do
+      mp3 = FactoryGirl.create(:micropost,
+              :user => FactoryGirl.create(:user, :email => FactoryGirl.generate(:email)))
+      @user.feed.include?(mp3).should be_falsey
+    end
   end
 end
