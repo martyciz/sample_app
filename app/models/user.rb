83283f57df4222b4ca8505bf77ddc61a0a039c0b
Martin Ciz <martyciz@gmail.com>
2014-07-22 20:20:30 +0200

    added micropost model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@ class User < ActiveRecord::Base
 
 	attr_accessor :password
 
+	has_many :microposts, :dependent => :destroy
+
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
 
 	validates :name, :presence => true,
--- spec/factories.rb
+++ spec/factories.rb
@@ -4,8 +4,19 @@ FactoryGirl.define do
 		email "mhartl@example.com"
 		password "foobar"
 		password_confirmation "foobar"
+
+		factory :user_with_micropost do
+			after_create do |user|
+				create(:micropost, user: user)
+			end
+		end 
 	end
 
 	sequence(:email) { |n| "person-#{n}@example.com" }
+
+	factory :micropost do
+		content "Foo bar"
+		association :user
+	end
 end
 
